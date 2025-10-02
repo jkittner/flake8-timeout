fd6a29998da421f0b78e9c91bda9a523db646bc8
Jonas Kittner <jonas.kittner@rub.de>
2025-10-02 12:40:35 +0200

    drop python 3.8

--- flake8_timeout.py
+++ flake8_timeout.py
@@ -1,10 +1,7 @@
 import ast
 import importlib.metadata as importlib_metadata
+from collections.abc import Generator
 from typing import Any
-from typing import Generator
-from typing import List
-from typing import Tuple
-from typing import Type
 
 MSG = 'TIM100 request call has no timeout'
 
@@ -16,7 +13,7 @@ METHODS = [
 
 class Visitor(ast.NodeVisitor):
     def __init__(self) -> None:
-        self.assignments: List[Tuple[int, int]] = []
+        self.assignments: list[tuple[int, int]] = []
 
     def visit_Call(self, node: ast.Call) -> None:
         if (
@@ -67,7 +64,7 @@ class Plugin:
     def __init__(self, tree: ast.AST):
         self._tree = tree
 
-    def run(self) -> Generator[Tuple[int, int, str, Type[Any]], None, None]:
+    def run(self) -> Generator[tuple[int, int, str, type[Any]], None, None]:
         visitor = Visitor()
         visitor.visit(self._tree)
         for line, col in visitor.assignments:
